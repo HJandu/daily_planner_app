5fcbf8da1ff12034d4a38989ed192bade9186550
Hardip Jandu <hardip_jandu.com>
2024-01-23 16:57:30 +0000

    included reset button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
+// display current date
 var currentDate = dayjs();
 $("#currentDay").text(currentDate.format("dddd, DD MMMM YYYY"));
 
+// change color of time blocks based on current time
 $(".time-block").each(function () {
     var currentHour = currentDate.hour();
     var blockHour = parseInt($(this).attr("id"));
@@ -13,15 +16,26 @@ $(".time-block").each(function () {
     }
 });
 
+// save button click listener
 $(".saveBtn").on("click", function () {
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, value);
 });
 
+// load any saved data from localStorage
 $(".time-block").each(function () {
     var time = $(this).attr("id");
     console.log(time);
     var value = localStorage.getItem(time);
     $(this).children(".description").val(value);
 });
+
+// clear past events
+$("#clear").on("click", function () {
+    alert("All events have been cleared!");
+    localStorage.clear();
+    $(".description").val("");
+
+});
+
